bc4a84064d06076fcabeab8563896542e20a3011
Xin Tai <xintainoble@gmail.com>
2016-10-02 18:14:47 +0300

    chore: add new class to get bundle

---
+++ Classes/USStateAbbreviations.h
@@ -0,0 +1,13 @@
+//
+//  USStateAbbreviations.h
+//  Pods
+//
+//  Created by iOS Developer on 10/2/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface USStateAbbreviations : NSObject
+
+@end
---
+++ Classes/USStateAbbreviations.m
@@ -0,0 +1,13 @@
+//
+//  USStateAbbreviations.m
+//  Pods
+//
+//  Created by iOS Developer on 10/2/16.
+//
+//
+
+#import "USStateAbbreviations.h"
+
+@implementation USStateAbbreviations
+
+@end
